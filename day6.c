4779deeae3249da4abc161866d31cba04e94c321
kimseonguk <ksg3941234@gmail.com>
2022-02-25 14:45:27 +0900

    test

--- day6.c
+++ day6.c
@@ -234,6 +234,6 @@ int main() {
 		{"영어", "이순신", 18000},
 		{"수학1", "강감찬", 10000}
 	};
-	puts("각 교과서의 이름은 다음과  같습니다.");
+	puts("각 교과서의 이름은 다음과   같습니다.");
 	printf("%s, %s, %s\n", text_book[0].title, text_book[1].title, text_book[2].title);
 }
