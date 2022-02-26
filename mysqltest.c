985bbf26961ae27ba8ca71666206d41abb573eff
kimseonguk <ksg3941234@gmail.com>
2022-02-27 00:56:26 +0900

    mysqltest

---
+++ mysqltest.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "mysql.h"
+
+int main(int argc, char* agrv[])
+{
+    MYSQL conn_ptr;        //connect DB
+    MYSQL_RES* res;        //result
+    MYSQL_ROW row;
+    int fields;
+    int cnt;
+
+    mysql_init(&conn_ptr);    //mssql initialize   
+
+    //sql object, host, user, passwd, db, port, socket, flag
+    //connect to server
+    //return pointer at success, return NULL at failed
+    if (!mysql_real_connect(&conn_ptr, "127.0.0.1", "root", "q1w2", "yk3", 0, NULL, 0))
+    {
+        printf("%s\n", mysql_error(&conn_ptr));
+        exit(1);
+    }
+
+    //return 0 al success
+    if (mysql_query(&conn_ptr, "select * from  connectLog"))
+    {
+        printf("%s\n", mysql_error(&conn_ptr));
+        exit(1);
+    }
+
+    //receive result value at once
+    res = mysql_store_result(&conn_ptr);
+
+    //get fields numbers
+    fields = mysql_num_fields(res);
+
+    //get 1 ROW in result, field is array type
+    while ((row = mysql_fetch_row(res)))
+    {
+        for (cnt = 0; cnt < fields; ++cnt)
+            printf("%s  ", row[cnt]);
+
+        printf("\n");
+    }
+
+    mysql_free_result(res);
+    mysql_close(&conn_ptr);    //close server, memory free
+
+    return 0;
+}
